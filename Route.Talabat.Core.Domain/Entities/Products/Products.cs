b9cdce0c6da082afa627b969e047515231cfe3d4
TECH VALLEY <TECH VALLEY@DESKTOP-CUMGKU2>
2024-10-05 14:28:21 +0300

    Part03 Product Module -Entitiy

---
+++ Route.Talabat.Core.Domain/Entities/Products/Products.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route.Talabat.Core.Domain.Entities.Products
+{
+    public class Products
+    {
+        public required string Name { get; set; }
+
+        public required string Description { get; set; }
+
+        public string ? PictureUrl { get; set; }
+
+        public decimal Price { get; set; }
+
+        public ProductBrand ? Brand { get; set; }
+
+        public int ? BrandId { get; set; }
+
+        public ProductCategory ? Category { get; set; }
+
+        public int? CategoryId { get; set; }
+
+
+    }
+}
