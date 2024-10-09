d7e53d2288a39b86136d4b19dbf14c587a75c2fc
TECH VALLEY <TECH VALLEY@DESKTOP-CUMGKU2>
2024-10-09 10:17:17 +0300

    Part04 Sevice Manager

--- Route.Talabat.Core.Application/Services/ServiceManager.cs
+++ Route.Talabat.Core.Application/Services/ServiceManager.cs
@@ -14,10 +14,17 @@ namespace Route.Talabat.Core.Application.Services
     internal class ServiceManager : IServiceManager
     {
         private readonly Lazy<IProductService> _productService;
+        private readonly IUnitOfWork unitOfWork;
+        private readonly IMapper mapper;
+
         public ServiceManager(IUnitOfWork unitOfWork, IMapper mapper)
         {
+            
             _productService=new Lazy<IProductService>(()=>new ProductService(unitOfWork,mapper));
+            this.unitOfWork = unitOfWork;
+            this.mapper = mapper;
         }
         public IProductService ProductService => _productService.Value;
+
     }
 }
