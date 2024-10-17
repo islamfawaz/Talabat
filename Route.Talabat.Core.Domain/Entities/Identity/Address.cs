52c817036a820c64465d432580393fda989cc7ae
fawazislam70@gmail.com <fawazislam70@gmail.com>
2024-10-17 15:44:55 +0100

    Part01 Identity Module -Entities

---
+++ Route.Talabat.Core.Domain/Entities/Identity/Address.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route.Talabat.Core.Domain.Entities.Identity
+{
+    public class Address
+    {
+        public required int Id { get; set; }
+
+        public required string FName { get; set; }
+        public required string LName { get; set; }
+
+        public required string Street { get; set; }
+
+        public required string City { get; set; }
+
+        public required string Country { get; set; }
+
+
+        public int UserId { get; set; }
+        public required ApplicationUser AppUser { get; set; }
+
+
+
+
+    }
+}
---
+++ Route.Talabat.Core.Domain/Entities/Identity/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route.Talabat.Core.Domain.Entities.Identity
+{
+    public class ApplicationUser :IdentityUser
+    {
+        public required string DisplayName { get; set; }
+
+        public Address ? Address { get; set; }
+    }
+}
