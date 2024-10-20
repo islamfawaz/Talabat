1a2af285d48b9da53482b82700a99b71e022b4da
TECH VALLEY <TECH VALLEY@DESKTOP-CUMGKU2>
2024-10-20 04:43:43 +0300

    Part03- Manage users

---
+++ Route.Talabat.Dashboard/Controllers/UserController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Route.Talabat.Core.Domain.Entities.Identity;
+using Route.Talabat.Dashboard.Models;
+
+namespace Route.Talabat.Dashboard.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userManager.Users.Select(U => new UserViewModel
+            {
+                Id = U.Id,
+                DisplayName = U.DisplayName,
+                UserName = U.UserName!,
+                PhoneNumber = U.PhoneNumber!,
+                Email = U.Email!,
+            }).ToListAsync();
+
+            foreach (var user in users)
+            {
+                user.Roles = await _userManager.GetRolesAsync(await _userManager.FindByIdAsync(user.Id));
+            }
+
+            return View(users);
+        }
+
+        public async Task<IActionResult> Edit(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            var roles = await _roleManager.Roles.ToListAsync();
+
+            var viewModel = new UserRolesViewModel
+            {
+                UserId = user!.Id,
+                UserName = user.UserName!,
+                Roles = roles.Select(
+                    async r => new RoleViewModel
+                    {
+                        Id = r.Id,
+                        Name = r.Name!,
+                        IsSelected = await _userManager.IsInRoleAsync(user, r.Name!)
+                    }).Select(t => t.Result).ToList() 
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(string id, UserRolesViewModel model)
+        {
+            var user = await _userManager.FindByIdAsync(model.UserId);
+            var userRoles = await _userManager.GetRolesAsync(user!);
+            var selectedRoles = model.Roles.Where(r => r.IsSelected).Select(r => r.Name).ToList();
+
+            // شيل الأدوار اللي المستخدم ما اختارهاش
+            var rolesToRemove = userRoles.Except(selectedRoles).ToList();
+            foreach (var role in rolesToRemove)
+            {
+                await _userManager.RemoveFromRoleAsync(user!, role);
+            }
+
+            // ضيف الأدوار الجديدة اللي المستخدم اختارها
+            var rolesToAdd = selectedRoles.Except(userRoles).ToList();
+            foreach (var role in rolesToAdd)
+            {
+                await _userManager.AddToRoleAsync(user!, role);
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Route.Talabat.Dashboard/Models/UserViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Route.Talabat.Dashboard.Models
+{
+    public class UserViewModel
+    {
+        public string Id { get; set; } = null!;
+        public string UserName { get; set; } = null!;
+
+        public string DisplayName { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public string PhoneNumber { get; set; } = null!;
+
+        public IEnumerable<string> Roles { get; set; }= null!;
+
+
+    }
+}
