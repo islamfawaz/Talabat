0516fd631e84ce64b30b2a90ca0e579bd2a42ef8
Islam Fawaz <150162386+islamfawaz@users.noreply.github.com>
2024-10-27 12:53:15 +0000

    Treatment Conflict02

--- Route.Talabat.Infrastructure.Persistance/_Identity/StoreIdentityDbInitializer.cs
+++ Route.Talabat.Infrastructure.Persistance/_Identity/StoreIdentityDbInitializer.cs
@@ -2,8 +2,52 @@
 using Route.Talabat.Core.Domain.Contract.Persistence.DbInitializer;
 using Route.Talabat.Core.Domain.Entities.Identity;
 using Route.Talabat.Infrastructure.Persistance.Common;
-using System.Threading.Tasks;
+using Route.Talabat.Infrastructure.Persistance.Identity;
 
-namespace Route.Talabat.Infrastructure.Persistance.Identity
+public sealed class StoreIdentityDbInitializer : DbInitializer, IStoreIdentityDbInitializer
 {
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
+
+    public StoreIdentityDbInitializer(StoreIdentityDbContext dbContext, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        : base(dbContext)
+    {
+        _userManager = userManager;
+        _roleManager = roleManager;
+    }
+
+    public override async Task SeedAsnc()
+    {
+        if (!await _roleManager.RoleExistsAsync("Admin"))
+        {
+            var adminRole = new IdentityRole("Admin");
+            await _roleManager.CreateAsync(adminRole);
+        }
+
+        var user = await _userManager.FindByEmailAsync("islam5@gmail.com");
+        if (user == null)
+        {
+            user = new ApplicationUser
+            {
+                DisplayName = "Islam Fawaz",
+                UserName = "islam.ahmed",
+                Email = "islam5@gmail.com",
+                PhoneNumber = "0112334455"
+            };
+
+            var result = await _userManager.CreateAsync(user, "P@ssw0rd");
+
+            if (result.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(user, "Admin");
+            }
+        }
+        else
+        {
+            if (!await _userManager.IsInRoleAsync(user, "Admin"))
+            {
+                await _userManager.AddToRoleAsync(user, "Admin");
+            }
+        }
+    }
 }
